d2e9f4e948a05faa25c76e77066a51fcbff4b1bc
skworkflow412 <skworkflow01@gmail.com>
2025-02-23 18:06:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -66,9 +66,9 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'vjcollection')
 MULTIPLE_DATABASE = bool(environ.get('MULTIPLE_DATABASE', False)) # Set True or False
 
 # If Multiple Database Is True Then Fill All Three Below Database Uri Else You Will Get Error.
-O_DB_URI = environ.get('O_DB_URI', "")   # This Db Is For Other Data Store
-F_DB_URI = environ.get('F_DB_URI', "")   # This Db Is For File Data Store
-S_DB_URI = environ.get('S_DB_URI', "")   # This Db is for File Data Store When First Db Is Going To Be Full.
+O_DB_URI = environ.get('O_DB_URI', "mongodb+srv://autofilterbot:autofilterbot@autofilterbot.egayw.mongodb.net/?retryWrites=true&w=majority&appName=autofilterbot")   # This Db Is For Other Data Store
+F_DB_URI = environ.get('F_DB_URI', "mongodb+srv://autofilterbot:autofilterbot@autofilterbot.um6b8.mongodb.net/?retryWrites=true&w=majority&appName=autofilterbot")   # This Db Is For File Data Store
+S_DB_URI = environ.get('S_DB_URI', "mongodb+srv://autofilterbot:autofilterbot@autofilterbot.utwlw.mongodb.net/?retryWrites=true&w=majority&appName=autofilterbot")   # This Db is for File Data Store When First Db Is Going To Be Full.
 
 
 # Premium And Referal Settings
