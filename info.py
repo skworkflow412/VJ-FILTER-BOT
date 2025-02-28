e18ca3b1887b10b00dbe3fc277a41ce5c4c4ab82
skworkflow412 <skworkflow01@gmail.com>
2025-02-28 18:57:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -26,7 +26,7 @@ auth_users = [int(user) if id_pattern.search(user) else user for user in environ
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 
 # This Channel Is For When User Start Your Bot Then Bot Send That User Name And Id In This Log Channel, Same For Group Also.
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002171340177'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002171340177 -1002419563985'))
 
 # This Is File Channel Where You Upload Your File Then Bot Automatically Save It In Database 
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002158569707 -1002416857882 -1002313759402 -1002316662569 -1002498472096').split()]  # For Multiple Id Use One Space Between Each.
